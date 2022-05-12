1656662040b3d8ea9e47c677603efd994f95323c
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-05-11 22:37:01 -0300

    Add files via upload

---
+++ Pratica5_LP/src/questao2/RedeSocial.java
@@ -0,0 +1,76 @@
+package questao2;
+
+import javax.swing.JOptionPane;
+import java.util.*;
+
+public class RedeSocial {
+
+	public static void main(String[] args) {
+		
+		 int i,j;
+	     ArrayList<String> RedeSocial = new ArrayList();
+
+	    // [ B ] usando o método add() para gravar 4 contatos na agenda
+	    
+	     do
+	     {
+	     String menu= JOptionPane.showInputDialog("MENU\n1-Cadastrar\n2-Excluir\n3-Pesquisar\n4-Sair");
+	     i = Integer.parseInt(menu);
+	     switch (i)
+	        {
+	             case 1:
+	                String nome= JOptionPane.showInputDialog("Digite o Nome: ");
+	                String telefone = JOptionPane.showInputDialog("Digite o Telefone: ");
+	                String usuarioinsta = JOptionPane.showInputDialog("Digite o @ do seu instagram: ");
+	                String email = JOptionPane.showInputDialog("Digite seu email: ");
+
+	                RedeSocial.add(nome+";"+telefone + ";" + usuarioinsta + ";" + email);
+	                JOptionPane.showMessageDialog(null, "Inserido com Sucesso");
+	                 break;
+	           
+	             case 2:
+	            	 String nomeexcluir = JOptionPane.showInputDialog("Digite o Nome: ");
+		               for( j = 0; j<RedeSocial.size(); j++)
+		               {
+		            	   if(RedeSocial.get(j).contains(nomeexcluir))
+		            	   {
+		            		   RedeSocial.remove(j);
+		            		   JOptionPane.showMessageDialog(null, "Removido com sucesso");
+		            	   }
+		               }
+		              break;
+	        
+	                      
+	             case 3:
+	            	 int x = RedeSocial.size();
+	            	 for(int y = 0; y < x ; y++) {
+	            		 String consultar = JOptionPane.showInputDialog("Insira sua Consulta");
+	            		 if(RedeSocial.get(y).contains(consultar)) {
+	            			 RedeSocial.get(y);
+	            			 JOptionPane.showMessageDialog(null, RedeSocial.get(y));
+	            			 }
+	            	 }
+	 
+	                   break;
+	                    
+	                    
+	             default:
+	            	 JOptionPane.showMessageDialog(null, "Opção Invalida");
+	       
+	              
+	        }
+	     }while(i!=4);
+	        
+	}
+}
+	     
+	     
+	     
+	
+	 
+
+	     
+
+	
+
+
